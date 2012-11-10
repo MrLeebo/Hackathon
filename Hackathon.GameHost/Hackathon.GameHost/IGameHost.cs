04a8a0a809a03badc8f888c26dc29c6690d04fe1
Ionut Ciordas <ionut.ciordas@gmail.com>
2012-11-10 12:25:53 -0600

    Renamed IGameHost

---
+++ Hackathon.GameHost/Hackathon.GameHost/IGameHost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hackathon.GameHost.Domain;
+
+namespace Hackathon.GameHost
+{
+    public interface IGameHost
+    {
+        event EventHandler<JSONEventArgs> OnPlayerJoined;
+        event EventHandler<JSONEventArgs> OnPlayerGuessSubmitted;
+        void StartRound(IEnumerable<Player> players, GameImage image, GameRound round);
+        void UpdatePlayerStatus(Player player, GameRound round, string status);
+        void StartJudgingRound();
+        
+    }
+}
